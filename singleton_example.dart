1cc483703db3740a44cfe76dc617ec7dc94ff9b7
Mauricio Sánchez <msanzr94@gmail.com>
2025-11-11 15:16:36 -0300

    Add lazy and eager singleton implementations
    Implemented lazy and eager initialization for singleton pattern with NumberSingletonLazy and NumberSingletonEager classes.

--- singleton_example.dart
+++ singleton_example.dart
@@ -3,31 +3,52 @@
 /// The Singleton pattern ensures that a class has only one instance
 /// and provides a global point of access to that instance.
 
-class Singleton {
-  // Private constructor
-  Singleton._privateConstructor();
+void main() {
+  // I prefer lazy in this case because I don't have clear view on how and where I'm going to
+  // be using this singleton, so I don't have to use memory unnecesary
 
-  // Static instance variable
-  static final Singleton _instance = Singleton._privateConstructor();
+  final nextNumber = NumberSingletonLazy();
+  print(nextNumber.getNextNumber());
+  print(nextNumber.getNextNumber());
+  print(nextNumber.getNextNumber());
 
-  // Factory constructor that returns the same instance
-  factory Singleton() {
-    return _instance;
-  }
+  final nextNumberEager = NumberSingletonEager();
+  print(nextNumberEager.getNextNumber());
+  print(nextNumberEager.getNextNumber());
+}
 
-  // Example method
-  void doSomething() {
-    print('Singleton instance is doing something');
+/// Lazy Init Singleton
+class NumberSingletonLazy {
+  NumberSingletonLazy._internal();
+  static NumberSingletonLazy? _instance;
+  factory NumberSingletonLazy() => _instance ?? NumberSingletonLazy._internal();
+
+  int position = 0;
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  int getNextNumber() {
+    final nextNumber = numbers[position];
+    position += 1;
+    return nextNumber;
   }
 }
 
-void main() {
-  // Creating instances
-  var instance1 = Singleton();
-  var instance2 = Singleton();
+/// Eager Init Singleton
+class NumberSingletonEager {
+  NumberSingletonEager._internal();
+  static final NumberSingletonEager _instance =
+      NumberSingletonEager._internal();
 
-  // Both instances are the same
-  print('Are instances identical? ${identical(instance1, instance2)}');
-  
-  instance1.doSomething();
+  factory NumberSingletonEager() {
+    print('Create NumberSingletonEager');
+    return _instance;
+  }
+  int position = 0;
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  int getNextNumber() {
+    final nextNumber = numbers[position];
+    position += 1;
+    return nextNumber;
+  }
 }
